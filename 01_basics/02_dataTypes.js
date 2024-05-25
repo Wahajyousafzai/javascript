5a9576cc27b206a482586607a91e4e9ed168c493
Wahajyousafzai <161183135+Wahajyousafzai@users.noreply.github.com>
2024-05-25 15:26:31 +0500

    first commit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // Treat All js code as a newer version
+
+// alert( 3 + 3 ) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3);  // code readability should be high
+
+let name = "wahaj"
+let age = 18
+let isLoggedIn = "true"
+let check = String(isLoggedIn)
+console.log(check);
+/* DataTypes
+number = 0 => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined =>
+symbol => unique    
+
+*/
+//  object 
+console.log(typeof undefined);
+console.log(typeof null);
